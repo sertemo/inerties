2a3091e47b289964ad2955114ba125d0e3a89030
sertemo <tejedor.moreno@gmail.com>
2024-08-12 21:54:17 +0200

    cambiado a int32

--- graficar.py
+++ graficar.py
@@ -104,7 +104,7 @@ class GraficarSeccion:
     def _draw_rotated_rectangle(self,img, center, width, height, angle, color, thickness):
         # Construimos la caja del rectángulo
         box = cv.boxPoints(((center[0], center[1]), (width, height), angle))
-        box = int(box)  # Aseguramos que las coordenadas sean enteras
+        box = np.int32(box)  # Aseguramos que las coordenadas sean enteras
 
         # Dibujamos el rectángulo en la imagen
         cv.drawContours(img, [box], 0, color, thickness)
